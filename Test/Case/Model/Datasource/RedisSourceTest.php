89cc404bfcc048529104c89b4dcd6d16b93ba625
Mischa ter Smitten <mischa@tersmitten.nl>
2014-10-24 11:56:25 +0200

    Added some simple tests

--- Test/Case/Model/Datasource/RedisSourceTest.php
+++ Test/Case/Model/Datasource/RedisSourceTest.php
@@ -1,8 +1,8 @@
 <?php
-App::uses('RedisSource', 'Datasources.Model/Datasource');
+App::uses('RedisSource', 'Redis.Model/Datasource');
 
 /**
- * RedisSourceTest class
+ * Redis Source Test class
  *
  */
 class RedisSourceTest extends CakeTestCase {
@@ -23,6 +23,50 @@ class RedisSourceTest extends CakeTestCase {
  */
 	public function tearDown() {
 		parent::tearDown();
+	}
+
+/**
+ * testListSources method
+ *
+ * @return void
+ */
+	public function testListSources() {
+		$Source = new RedisSource();
+
+		$result = $Source->listSources();
+
+		$this->assertNull($result);
+	}
+
+/**
+ * testListSources method
+ *
+ * @return void
+ */
+	public function testDescribe() {
+		$Source = new RedisSource();
+		$Model = $this->getMockForModel('Model');
+
+		$result = $Source->describe($Model);
+
+		$this->assertNull($result);
+	}
+
+/**
+ * testListSources method
+ *
+ * @return void
+ */
+	public function testCalculate() {
+		$Source = new RedisSource();
+		$Model = $this->getMockForModel('Model');
+		$func = 'foo';
+		$params = array('b', 'a', 'r');
+
+		$result = $Source->calculate($Model, $func, $params);
+		$expected = array('count' => true);
+
+		$this->assertIdentical($expected, $result);
 	}
 
 }
